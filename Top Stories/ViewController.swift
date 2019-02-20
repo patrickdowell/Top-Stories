a10083cfe34c497fa1310cdca7a2f025fd20520e
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-20 11:15:15 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Patrick Dowell on 2/20/19.
+//  Copyright © 2019 Patrick Dowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
