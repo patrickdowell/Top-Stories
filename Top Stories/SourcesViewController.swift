0dbab95846f9574ba98ef07e83a19c9eb6c4ad25
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-21 11:17:21 -0600

    Finished initial set up of SourcesTableView

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -47,6 +47,18 @@ class SourcesViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 
 }
 
